d4221fbbdbc534eadcd7fb4e11910b965bef6867
xuqiangsheng <xuqiangsheng@rfchina.com>
2020-12-28 09:30:17 +0800

    Initial commit

---
+++ src/main/java/TestGit.java
@@ -0,0 +1,11 @@
+/**
+ * description:
+ *
+ * @author xuqiangsheng
+ * @date 2020/12/28 9:29
+ */
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("新建程序001");
+    }
+}
